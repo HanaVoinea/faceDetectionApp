63b703f874fff103f89fa82842f447d86b53bc89
Hana <61012894+HanaVoinea@users.noreply.github.com>
2021-03-06 02:04:39 +0200

    Add files via upload

---
+++ precache-manifest.144393bc3a2ca5e633b8b77e85d8e13a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cabd850b13084e2371107dbc179cdaae",
+    "url": "/faceDetectionApp/index.html"
+  },
+  {
+    "revision": "f0a9ed4c05ec44f64549",
+    "url": "/faceDetectionApp/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "8bd48f0e0bb669219825",
+    "url": "/faceDetectionApp/static/css/main.d2317254.chunk.css"
+  },
+  {
+    "revision": "f0a9ed4c05ec44f64549",
+    "url": "/faceDetectionApp/static/js/2.8f286cf2.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/faceDetectionApp/static/js/2.8f286cf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bd48f0e0bb669219825",
+    "url": "/faceDetectionApp/static/js/main.089623aa.chunk.js"
+  },
+  {
+    "revision": "67ee5f6b912f9768a505",
+    "url": "/faceDetectionApp/static/js/runtime-main.b7cf7af4.js"
+  }
+]);
